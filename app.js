8658fb122a7e6cca4a0a7d54771d8c7f94078b94
JUNEBOK LEE <junebok.lee@nlcbiz.co.kr>
2021-08-17 15:43:53 +0900

    Conditionals

--- app.js
+++ app.js
@@ -1,34 +1,15 @@
-const age = 96;
+const age = parseInt((prompt("How old are you?")));
 
-function calculateKrAge(AgeOfForeigner){
-   return AgeOfForeigner + 2;
+console.log(isNaN(age));
+
+if (isNaN (age) || age < 0) { // condition === true
+    console.log("Please write a real positive number");
+} else if (age < 18) { // condition === false
+    console.log("You are too young.");
+} else if (age >= 18 && age <= 50) {
+    console.log("You can drink");
+} else if (age > 50 && age <= 80) {
+    console.log("You should exercise");
+} else if (age > 80) {
+    console.log("You can do whatever you want");
 }
-
-const krAge = calculateKrAge(age);
-
-console.log(krAge);
-
-
-const calculator = {
-    plus : function (a,b) {
-        return a + b;
-    },
-    minus : function (a,b) {
-        return a - b;
-    },
-    multiply: function (a,b) {
-        return a * b;
-    },
-    divide : function (a,b) {
-        return a / b;
-    },
-    power : function (a,b) {
-        return a ** b;
-    }
-}
-
-const plusResult = calculator.plus(2,3);
-const minusResult = calculator.minus(plusResult, 10);
-const multiplyResult = calculator.multiply(10, minusResult);
-const divideResult = calculator.divide(multiplyResult, plusResult);
-const powerResult = calculator.power(divideResult,minusResult);
