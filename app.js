aa722cb63d5095244725745806b5e61f6da7d136
JUNEBOK LEE <junebok.lee@nlcbiz.co.kr>
2021-08-17 14:22:56 +0900

    Functions

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
-const dayOfWeek=["mon","tue","wed","thu","fri","sat","sun"];
+function sayHello(nameOfPerson, age){
+    console.log("Hello! My name is " + nameOfPerson+ "and I'm " + age);
+}
 
-console.log(dayOfWeek[6]);
+sayHello("Nico",10);
+sayHello("Lynn",20);
+sayHello("Jane",24);
+
+function Plus(a,b){
+    console.log(a+b);
+}
+
+Plus(8,60);
+Plus(15,32);
+Plus(13,90);
+
+const player = {
+    name: "Nico",
+    sayHello: function (otherPersonsName) {
+        console.log("Hello! "+otherPersonsName+ " nice to meet you!");
+    }
+};
+
+console.log(player.name);
+player.sayHello("Lynn");
+player.sayHello("June");
