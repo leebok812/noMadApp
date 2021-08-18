567d0af170bac16312f00df62e4338f7cbd30cba
JUNEBOK LEE <junebok.lee@nlcbiz.co.kr>
2021-08-18 17:13:11 +0900

    Events part one

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 const title = document.querySelector(".hello h1");
 
-console.log(title);
+
+function handleTitleClick () {
+    title.style.color = "blue"
+}
+
+
+
+title.addEventListener("click", handleTitleClick);
