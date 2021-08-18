b7dd0dd0ed766f792138030da1c1e56c8a9c382a
JUNEBOK LEE <junebok.lee@nlcbiz.co.kr>
2021-08-18 22:01:38 +0900

    CSS in JavaScript part Three

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const h1 = document.querySelector("div.hello:first-child h1");
 
 
 function handleTitleClick () {
-    const clickedClass ="clicked"
-    if(h1.className === "clickedClass"){
-        h1.className = "";
-    } else {
-        h1.className = "clickedClass";
-    }
+    // const clickedClass = "clicked";
+    // if (h1.classList.contains(clickedClass)) {
+    //     h1.classList.remove(clickedClass);
+    // } else {
+    //     h1.classList.add(clickedClass);
+    // }
+    h1.classList.toggle("clicked");
 }
 
 
