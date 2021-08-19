4a45f2f93859ed752340ead1be5ece9ccb192037
JUNEBOK LEE <junebok.lee@nlcbiz.co.kr>
2021-08-19 13:07:30 +0900

    Getting Username

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden";  // 일반적으로 String만 포함된 변수는 대문자로 표기하고 String을 저장하고 싶을 때 사용한다.
+
+// const link = document.querySelector("a");
 
 
 function onLoginSubmit (event) {
    // const username = loginInput.value;
    // console.log(username);
 
-   event.preventDefault(); // 어떤 event의 기본 행동이 발생되지 않도록 막음 -- 여기서는 form을 submit할경우 새로고침을 막아줌
-   console.log(loginInput.value);
+   event.preventDefault(); // 어떤 event의 기본 행동이 발생되지 않도록 막음 -- 여기서는 form을 submit할경우 새로고침을 막아줌 
+   loginForm.classList.add(HIDDEN_CLASSNAME);
+   const username = loginInput.value;
+   // greeting.innerText = "Hello " + username;
+   greeting.innerText = `Hello  ${username}`;
+   greeting.classList.remove(HIDDEN_CLASSNAME);
+
 
 
     // if(username === ""){
