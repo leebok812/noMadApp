c0470ccf0dc9bd8fe5738cadf5ef46da731c9ac4
JUNEBOK LEE <junebok.lee@nlcbiz.co.kr>
2021-08-19 15:53:38 +0900

    Adding ToDos

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,24 @@ const toDoForm = document.getElementById("todo-form");
 const toDoList = document.getElementById("todo-list");
 const toDoInput = document.querySelector("#todo-form input");
 
+function paintToDo (newTodo) {
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    
+    li.appendChild(span);
+    span.innerText = newTodo;
+    
+    toDoList.appendChild(li);
+
+}
+
+
 
 function hadleToDoSubmit (event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
+    paintToDo(newTodo);
 }
 
 toDoForm.addEventListener("submit",hadleToDoSubmit);
