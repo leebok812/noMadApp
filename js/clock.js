ef1bf3ca26158ada8964063bc2b1aa03cb60fa4d
JUNEBOK LEE <junebok.lee@nlcbiz.co.kr>
2021-08-19 14:28:08 +0900

    PadStart

--- js/clock.js
+++ js/clock.js
@@ -2,8 +2,11 @@ const clock = document.querySelector("h2#clock");
 
 function getClock () {
     const date = new Date();
+    const hours = String(date.getHours()).padStart(2,"0");
+    const minutes = String(date.getMinutes()).padStart(2,"0");
+    const seconds =  String(date.getSeconds()).padStart(2,"0");
 
-    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 getClock(); // 이게 없으면 1초 뒤에 실행됨
 setInterval(getClock, 1000);
