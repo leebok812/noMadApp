5eb5f7503f0d26177c5e490a470813d8ffd42c91
JUNEBOK LEE <junebok.lee@nlcbiz.co.kr>
2021-08-19 16:21:39 +0900

    Saving ToDos

--- js/todo.js
+++ js/todo.js
@@ -3,6 +3,14 @@ const toDoList = document.getElementById("todo-list");
 const toDoInput = document.querySelector("#todo-form input");
 
 
+const toDos = [];
+
+
+function saveToDos () {
+    localStorage.setItem("todos", JSON.stringify(toDos))
+}
+
+
 function deleteToDo (event) {
     const li = event.target.parentElement;
     li.remove();
@@ -28,7 +36,9 @@ function hadleToDoSubmit (event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
+    toDos.push(newTodo);
     paintToDo(newTodo);
+    saveToDos();
 }
 
 toDoForm.addEventListener("submit",hadleToDoSubmit);
