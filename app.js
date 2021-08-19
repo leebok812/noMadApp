0bc56ca16e16c93e4c7d10a806b6fde317f663f1
JUNEBOK LEE <junebok.lee@nlcbiz.co.kr>
2021-08-19 10:43:20 +0900

    Input Values

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
-const h1 = document.querySelector("div.hello:first-child h1");
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
 
-
-function handleTitleClick () {
-    // const clickedClass = "clicked";
-    // if (h1.classList.contains(clickedClass)) {
-    //     h1.classList.remove(clickedClass);
-    // } else {
-    //     h1.classList.add(clickedClass);
-    // }
-    h1.classList.toggle("clicked");
+function onLoginBtnClick () {
+    console.log("Hello", loginInput.value)
 }
 
 
-h1.addEventListener("click",handleTitleClick);
+loginButton.addEventListener("click", onLoginBtnClick);
