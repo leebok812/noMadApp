3aec90d25e95d56fd11f131fcee63a246347bbb8
JUNEBOK LEE <junebok.lee@nlcbiz.co.kr>
2021-08-19 16:07:44 +0900

    Deleting ToDos

--- js/todo.js
+++ js/todo.js
@@ -2,13 +2,22 @@ const toDoForm = document.getElementById("todo-form");
 const toDoList = document.getElementById("todo-list");
 const toDoInput = document.querySelector("#todo-form input");
 
+
+function deleteToDo (event) {
+    const li = event.target.parentElement;
+    li.remove();
+}
+
+
 function paintToDo (newTodo) {
     const li = document.createElement("li");
     const span = document.createElement("span");
-    
+    span.innerText = newTodo;   
+    const button = document.createElement("button");
+    button.innerText ="❌";
+    button.addEventListener("click",deleteToDo);
     li.appendChild(span);
-    span.innerText = newTodo;
-    
+    li.appendChild(button);
     toDoList.appendChild(li);
 
 }
