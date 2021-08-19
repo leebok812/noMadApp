6bf61aff95e202570951b17f338c92299660d315
JUNEBOK LEE <junebok.lee@nlcbiz.co.kr>
2021-08-19 22:04:58 +0900

    Weather API

--- js/weather.js
+++ js/weather.js
@@ -1,8 +1,16 @@
+const API_KEY = "5585b168e653d9453b7cce752514a27d";
+
 
 function onGeoOk (position) {
      const lat = position.coords.latitude;
-     const lng = position.coords.longitude;
-    console.log("You live in",lat,lng)
+     const lon = position.coords.longitude;
+     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+     fetch(url).then(Response => Response.json()).then(data =>{
+         const weather = document.querySelector("#weather span:first-child")
+         const city = document.querySelector("#weather span:last-child")
+            city.innerText  = data.name;
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+     })
 }
 
 function onGeoError () {
